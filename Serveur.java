d08f76673dbc7fe450c1f38b0f1ccc4663705fab
KHALI mehdi <khali.mehdi.km@gmail.com>
2022-04-08 13:39:29 +0200

    ajout d'1 multicast socket / traitement des requêtes GAMES OGAME NEWPL REGIS / [A CORRIGER]

--- Serveur.java
+++ Serveur.java
@@ -4,22 +4,72 @@ import java.util.ArrayList;
 
 public class Serveur {
 
-    ArrayList<Partie> liste = new ArrayList<Partie>();
+    ArrayList<Partie> liste = new ArrayList<Partie>(); 
+    
+    public boolean enregistre_joueur (String joueur_id, int port, int m) {
+        for (Partie p : liste) {
+            if (p.id == m) {
+                p.liste.add(new Joueur(joueur_id, port));
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         try {
             ServerSocket serv = new ServerSocket(9999);
+            MulticastSocket mso = new MulticastSocket(12500);
             while (true) {
                 Socket sock = serv.accept();
                 BufferedReader lire = new BufferedReader(new InputStreamReader(sock.getInputStream()));
                 PrintWriter ecrit = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+                
+                ecrit.println("GAMES " + String.valueOf(liste.size()) + "***");
+                ecrit.flush();
+                for (Partie p : liste) {
+                    ecrit.println("OGAME " + String.valueOf(p.id) + String.valueOf(p.liste.size()) + "***");
+                    ecrit.flush();
+                }
+                String mess = lire.readLine();
+                if (mess.contains("NEWPL")) {
+                    String[] instruction = mess.split(' ');
+                    if (instruction.length != 3) {
+                        ecrit.println("REGNO***");
+                        ecrit.flush();
+                    }
+                    else {
+                        pnew = new Partie();
+                        liste.add(pnew);
+                        pnew.liste.add(new Joueur(instruction[1], instruction[2]));
+                        ecrit.println("REGOK " + pnew.id + "***");
+                        ecrit.flush();
+                    }
+                }
+                
+                else if (mess.contains("REGIS")) {
+                    String[] instruction = mess.split(' ');
+                    if (instruction.length != 4) {
+                        ecrit.println("REGNO***");
+                        ecrit.flush();
+                    }
+                    boolean enregistre = enregistre_joueur(instruction[1], instruction[2], instruction[3]);
+                    if (boolean) {
+                        ecrit.println("REGOK " + pnew.id + "***");
+                        ecrit.flush();
+                    }
+                    else {
+                        ecrit.println("REGNO***");
+                        ecrit.flush();
+                    }
+                }
+                
 
                 lire.close();
                 ecrit.close();
                 sock.close();
             }
         } catch (Exception e) {
-            System.out.println(e);
             e.printStackTrace();
         }
     }
