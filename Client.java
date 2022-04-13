392de6b510bbed2851d5d7810fa7ed93d8502ac7
KHALI mehdi <khali.mehdi.km@gmail.com>
2022-04-13 12:38:07 +0200

    Creation du client

---
+++ Client.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+
+public class Serveur {
+
+    public static void main (String[]args) {
+        try {
+            Socket sock = new Socket("localhost",9999); // ADAPTER POUR LULU
+            BufferedReader lire = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            PrintWriter ecrit = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+            while(true) {
+                String mess = br.readLine();
+                System.out.println(mess);
+            }
+            lire.close();
+            ecrit.close();
+            sock.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
