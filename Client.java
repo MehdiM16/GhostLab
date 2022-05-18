886d2d44055e40f5b48f135ab56cd6caba426a5c
immoune <g.immoune94@gmail.com>
2022-05-19 00:35:31 +0200

    changement localhost

--- Client.java
+++ Client.java
@@ -326,7 +326,7 @@ public class Client {
 
     public static void main(String[] args) {
         try {
-            Socket sock = new Socket("192.168.70.236", 9999); // ADAPTER POUR LULU
+            Socket sock = new Socket("localhost", 9999); // ADAPTER POUR LULU
             BufferedReader lire = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             PrintWriter ecrit = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
             Scanner sc = new Scanner(System.in);
