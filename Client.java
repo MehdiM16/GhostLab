e0089b147df97582a39073f8c84ccebbb6b2ef95
immoune <g.immoune94@gmail.com>
2022-05-21 19:03:36 +0200

    test

--- Client.java
+++ Client.java
@@ -378,7 +378,7 @@ public class Client {
 
     public static void main(String[] args) {
         try {
-            Socket sock = new Socket("192.168.70.236", 9123); // ADAPTER POUR LULU
+            Socket sock = new Socket("localhost", 9123); // ADAPTER POUR LULU
             BufferedReader lire = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             PrintWriter ecrit = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
             Scanner sc = new Scanner(System.in);
